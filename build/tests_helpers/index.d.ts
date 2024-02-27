ee4afcfa6d9ca37ac62be9782174f957dec54a6c
Eric Ingram <eric@swell.is>
2024-02-27 16:00:56 -0700

    add build for testing

---
+++ build/tests_helpers/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="node" resolution-mode="require"/>
+export declare const BASE_URL: import("url").URL;
+export declare function normalizeNewLines(value: string): string;
+export declare function normalizeFilename(basePath: string, value: string): string;
